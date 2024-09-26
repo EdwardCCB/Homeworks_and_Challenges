b1c30bfb2a3e2fc3fb19a04fbf5a70c5698bad5d
Eduardo Marino <eduardomarino1028@hotmail.com>
2024-09-25 21:42:15 -0500

    Create FirstApp component and update App

---
+++ aplication/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FirstApp from './components/FirstApp.js';
+
+function App() {
+  return (
+    <div>
+      <FirstApp />
+    </div>
+  );
+}
+
+export default App;
